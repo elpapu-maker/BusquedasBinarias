07987b48243505f11c1a2513df69c2ea99779d7e
Development <Development@192.168.218.1>
2019-12-03 19:34:55 -0600

    Revert ""
    This reverts commit 9908efff7e43045ffa08a03b7a3a94a5d48440d8.

--- src/principal/Interpolacion.java
+++ src/principal/Interpolacion.java
@@ -10,23 +10,28 @@ package principal;
  * @author Development
  */
 public class Interpolacion {
-    public static int BusquedaInterpolacion(int[] numeroEntero, int ElementoBuscado) {
-    int Primero = 0;
-    int ultimo = (numeroEntero.length - 1);
-    while ((Primero <= ultimo) && (ElementoBuscado >= numeroEntero[Primero]) &&
-           (ElementoBuscado <= numeroEntero[ultimo])) {
+    public static int interpolationSearch(int[] integers, int elementToSearch) {
+    int startIndex = 0;
+    int lastIndex = (integers.length - 1);
+    while ((startIndex <= lastIndex) && (elementToSearch >= integers[startIndex]) &&
+           (elementToSearch <= integers[lastIndex])) {
         // using interpolation formulae to find the best probable position for this element to exist
-        int n = Primero + (((ultimo-Primero) /
-          (numeroEntero[ultimo]-numeroEntero[Primero]))*
-                        (ElementoBuscado - numeroEntero[Primero]));
-        if (numeroEntero[n] == ElementoBuscado)
-            return n;
-        if (numeroEntero[n] < ElementoBuscado)
-            Primero = n + 1;
+        int pos = startIndex + (((lastIndex-startIndex) /
+          (integers[lastIndex]-integers[startIndex]))*
+                        (elementToSearch - integers[startIndex]));
+        if (integers[pos] == elementToSearch)
+            return pos;
+        if (integers[pos] < elementToSearch)
+            startIndex = pos + 1;
         else
-            ultimo = n - 1;
+            lastIndex = pos - 1;
     }
     return -1;
 }
+
+    static int Busqueda(int[] vector, int dato) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
 
 }
